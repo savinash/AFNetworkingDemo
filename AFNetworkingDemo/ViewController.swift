0096f5bba20d1d87f435c2cf4a7d9ee6d3d9a800
Dwellze <jagpritbatra@gmail.com>
2022-05-06 12:47:55 +0100

    Initial Commit

---
+++ AFNetworkingDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AFNetworkingDemo
+//
+//  Created by Avinash Kumar on 06/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
